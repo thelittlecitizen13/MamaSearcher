a265a081d02a93dd2b942c433681bf1037e9a120
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-23 17:06:34 +0300

    Program tested

--- MamaSearcher/Program.cs
+++ MamaSearcher/Program.cs
@@ -6,7 +6,12 @@ namespace MamaSearcher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MamaSearcher ms = new MamaSearcher();
+            ms.Subscribe("abc", ActionsToPerform.PrintIndexAtContent);
+            ms.Subscribe("yuval", ActionsToPerform.PrintIndexAtContent);
+            ms.Subscribe("assa", ActionsToPerform.PrintIndexAtContent);
+            ms.Subscribe("amit", ActionsToPerform.PrintIndexAtContent);
+            ms.PerformSearch("amitassa");
         }
     }
 }
