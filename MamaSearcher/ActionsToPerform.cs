155403802d3b31c2916a3024fb74e346f973b1af
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-23 17:03:05 +0300

    MamaSearcher and ActionsToPerform classes added

---
+++ MamaSearcher/ActionsToPerform.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MamaSearcher
+{
+    public class ActionsToPerform
+    {
+        public static void PrintIndexAtContent(int index, string content)
+        {
+            Console.WriteLine($"Content '{content}' contains a pattern at index {index}.");
+        }
+    }
+}
---
+++ MamaSearcher/MamaSearcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MamaSearcher
+{
+    class MamaSearcher : IMamaSearcher
+    {
+        private Dictionary<string, Action<int, string>> _patternToAction;
+        public MamaSearcher()
+        {
+            _patternToAction = new Dictionary<string, Action<int, string>>();
+        }
+        public void PerformSearch(string content)
+        {
+            if (_patternToAction.Count == 0)
+            {
+                return;
+            }
+            foreach (var pair in _patternToAction)
+            {
+                int index = content.IndexOf(pair.Key);
+                if (index != -1)
+                {
+                    pair.Value.Invoke(index, content);
+                }
+            }
+        }
+
+        public void Subscribe(string pattern, Action<int, string> actionToPerform)
+        {
+            if (!String.IsNullOrWhiteSpace(pattern) && actionToPerform != null)
+                _patternToAction.Add(pattern, actionToPerform);
+        }
+    }
+}
